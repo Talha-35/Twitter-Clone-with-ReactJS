2458eff8ba59ac1c4340b988cc4da687e85c8ed3
Talha-35 <67706755+Talha-35@users.noreply.github.com>
2020-12-16 23:55:51 +0100

    app.js modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import './App.css';
+import "./App.css";
+// import Feed from "./Feed";
+import Sidebar from "./Sidebar";
+import Widgets from "./Widgets";
 
 function App() {
   return (
-    <div className="App">
-   
+    <div className="app">
+      <Sidebar />
+      {/* <Feed /> */}
+      <Widgets />
     </div>
   );
 }
